9d056af63737ccf7e312486f65ac089cc806b77f
Kalpesh300 <kalpesh@auberginesolutions.com>
2021-01-02 13:29:50 +0530

    [ADD] Bootstrap Navigation panel and routes for the application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationPanelComponent } from './components/navigation-panel/navigation-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationPanelComponent
   ],
   imports: [
     BrowserModule,
